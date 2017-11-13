04d32e631102c94ce70c472cae7eafc28f0f2ca9
Rileygit push <rileylnapier@gmail.com>
2017-11-12 17:18:15 -0800

    push to origin

--- index.js
+++ index.js
@@ -4,12 +4,15 @@ var fs = require('fs');
 var path = require('path');
 
 module.exports = function(source) {
-	var options = loaderUtils.getOptions(this);
-	options.readFileSync = (fileName) => {
-		const filePath = path.resolve(path.dirname(this.resourcePath), fileName);
-		this.addDependency(filePath);
-		return fs.readFileSync(filePath);
-	}
-	this.cacheable && this.cacheable();
-	return reactTemplates.convertTemplateToReact(source, options);
+  var options = loaderUtils.getOptions(this);
+  if (!options.readFileSync) {
+    options.readFileSync = fileName => {
+      const filePath = path.resolve(path.dirname(this.resourcePath), fileName);
+      this.addDependency(filePath);
+      return fs.readFileSync(filePath);
+    };
+  }
+
+  this.cacheable && this.cacheable();
+  return reactTemplates.convertTemplateToReact(source, options);
 };
